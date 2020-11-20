c53129c43aaf772c686a12dd7b1542cdd25980a6
Patrick Zhang <patrick_zhang1@brown.edu>
2020-11-19 22:15:52 -0500

    Adding champ data

---
+++ src/ChampData.js
@@ -0,0 +1,94 @@
+const AllChampData = [
+  {
+    name: "Seraphine",
+    class: Set(["Mage", "Controller"]),
+    role: Set(["Middle", "Support"]),
+    price: 6300
+  },
+  {
+    name: "Yasuo",
+    class: Set(["Slayer"]),
+    role: Set(["Top", "Middle", "Bottom"]),
+    price: 4800
+  },
+  {
+    name: "Ornn",
+    class: Set(["Tank"]),
+    role: Set(["Top"]),
+    price: 6300
+  },
+  {
+    name: "Janna",
+    class: Set(["Controller"]),
+    role: Set(["Support"]),
+    price: 1350
+  },
+  {
+    name: "Jax",
+    class: Set(["Fighter"]),
+    role: Set(["Top", "Jungle"]),
+    price: 1350
+  },
+  {
+    name: "Ashe",
+    class: Set(["Marksman"]),
+    role: Set(["Bottom"]),
+    price: 450
+  },
+  {
+    name: "Galio",
+    class: Set(["Mage", "Tank"]),
+    role: Set(["Middle, Support"]),
+    price: 3150
+  },
+  {
+    name: "Garen",
+    class: Set(["Fighter, Tank"]),
+    role: Set(["Top", "Middle"]),
+    price: 450
+  },
+  {
+    name: "Pantheon",
+    class: Set(["Fighter"]),
+    role: Set(["Top", "Middle", "Support"]),
+    price: 3150
+  },
+  {
+    name: "Volibear",
+    class: Set(["Fighter"]),
+    role: Set(["Top", "Jungle"]),
+    price: 3150
+  },
+  {
+    name: "Samira",
+    class: Set(["Marksman"]),
+    role: Set(["Bottom"]),
+    price: 6300
+  },
+  {
+    name: "Lucian",
+    class: Set(["Marksman"]),
+    role: Set(["Top", "Middle", "Bottom"]),
+    price: 4800
+  },
+  {
+    name: "Bard",
+    class: Set(["Controller"]),
+    role: Set(["Support"]),
+    price: 6300
+  },
+  {
+    name: "Zed",
+    class: Set(["Slayer"]),
+    role: Set(["Middle"]),
+    price: 4800
+  },
+  {
+    name: "Katarina",
+    class: Set(["Slayer"]),
+    role: Set(["Middle"]),
+    price: 3150
+  }
+]
+
+export default AllChampData;
