2eaf17bb934d0ad9c6270c569bdec9ec64cee390
Patrick Zhang <patrick_zhang1@brown.edu>
2020-11-20 03:06:55 -0500

    Finished filtering and sorting logic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,110 @@
+import './App.css'; // load default ant design CSS so we can override later
+
 import React, { Component } from 'react';
 import { Layout, Typography } from 'antd';
 import Champion from './Champion';
-import FilterBar from './FilterBar';
+import FilterBar, { SortEnum } from './FilterBar';
 import AllChampData from './ChampData';
-import './App.css';
 
 const { Sider, Content } = Layout;
 const { Title } = Typography;
+
+const Classes = [
+  "Controller",
+  "Fighter",
+  "Mage",
+  "Marksman",
+  "Slayer",
+  "Tank"
+]
+
+const Roles = [
+  "Top",
+  "Jungle",
+  "Middle",
+  "Bottom",
+  "Support"
+]
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {champs: AllChampData};
+    const newChamps = [...AllChampData];
+    newChamps.sort((a, b) => a.name.localeCompare(b.name));
+
+    this.state = {
+      champs: newChamps,
+      classes: [],
+      roles: [],
+      order: SortEnum.NAME_ASC
+    };
   }
+
+  onClassFilter = (checkedValues) => {
+    this.setState({classes: checkedValues}, () => {
+      this.filterChamps();
+    })
+  }
+
+  onRoleFilter = (checkedValues) => {
+    this.setState({roles: checkedValues}, () => {
+      this.filterChamps();
+    })
+  }
+
+  filterChamps = () => {
+    let newChamps = [...AllChampData];
+    if (this.state.classes.length !== 0) {
+      newChamps = newChamps.filter((champ) => this.state.classes.every((target) => champ.class.includes(target)));
+    }
+    if (this.state.roles.length !== 0) {
+      newChamps = newChamps.filter((champ) => this.state.roles.every((target) => champ.role.includes(target)));
+    }
+    this.setState({champs: newChamps}, () => {
+      this.onSort(this.state.order);
+    });
+  }  
+
+  onSort = (fakeEnum) => {
+    // javascript sort mutates array, so we need to copy champs first
+    const newChamps = [...this.state.champs];
+
+    switch(fakeEnum) {
+      case SortEnum.NAME_ASC:
+        newChamps.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case SortEnum.NAME_DESC:
+        newChamps.sort((a, b) => -(a.name.localeCompare(b.name)));
+        break;
+      case SortEnum.COST_ASC:
+        newChamps.sort((a, b) => a.price - b.price)
+        break;
+      case SortEnum.COST_DESC:
+        newChamps.sort((a, b) => -(a.price - b.price))
+        break;
+      default:
+        newChamps.sort((a, b) => a.name.localeCompare(b.name));
+    }
+    this.setState({champs: newChamps, order: fakeEnum});
+  };
 
   render() {
     return (
       <>
-        <Title className="title">League of Legends Champion Explorer</Title>
+        <Title className="title">
+          League of Legends Champion Explorer
+        </Title>
         <Layout>
           <Sider>
-            <FilterBar />
+            <FilterBar
+              classes={Classes}
+              roles={Roles}
+              onClassFilter={this.onClassFilter}
+              onRoleFilter={this.onRoleFilter}
+              onSort={this.onSort}
+              defaultVal={SortEnum.NAME_ASC}
+            />
           </Sider>
           <Content className="flex">
             {
--- src/FilterBar.js
+++ src/FilterBar.js
@@ -1,4 +1,17 @@
 import React, { Component } from 'react';
+import { Space, Checkbox, Typography, Select } from 'antd';
+import "./FilterBar.css"
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+// fake enum since javascript doesn't have built in enums
+const SortEnum = {
+  NAME_ASC: "1",
+  NAME_DESC: "2",
+  COST_ASC: "3",
+  COST_DESC: "4"
+}
 
 class FilterBar extends Component {
   constructor(props) {
@@ -7,9 +20,22 @@ class FilterBar extends Component {
 
   render() {
     return (
-      <div>FilterBar</div>
+      <Space direction="vertical" className="flex-column">
+        <Title level={2} className="no-margin">Classes:</Title>
+        <Checkbox.Group options={this.props.classes} onChange={this.props.onClassFilter} />
+        <Title level={2} className="no-margin">Roles:</Title>
+        <Checkbox.Group options={this.props.roles} onChange={this.props.onRoleFilter} />
+        <Title level={2} className="no-margin">Sort By:</Title>
+        <Select defaultValue={this.props.defaultVal} onChange={this.props.onSort}>
+          <Option value={SortEnum.NAME_ASC}>Name Ascending</Option>
+          <Option value={SortEnum.NAME_DESC}>Name Descending</Option>
+          <Option value={SortEnum.COST_ASC}>Cost Ascending</Option>
+          <Option value={SortEnum.COST_DESC}>Cost Descending</Option>
+        </Select>
+      </Space>
     );
   }
 };
 
 export default FilterBar;
+export { SortEnum };
